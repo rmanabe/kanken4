63a9f5f96586e0e56705091db936e1ee07cc6f53
rmanabe <r_manabe@robonet-c.jp>
2026-03-19 21:19:37 +0900

    スマホのhover残留修正

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kanken4-v8';
+const CACHE = 'kanken4-v10';
 const FILES = ['./index.html', './app.js', './questions.js', './manifest.json'];
 
 self.addEventListener('install', e => {
